0b5c996f7ed8b33737c61c8ec3d1727ab3e7906d
Steamheh <mtony2508@gmail.com>
2025-06-28 17:31:50 +0300

    added booking and city model, login loader/manager and pass secret key

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 from flask import Flask
-from flask_login import UserMixin
+from flask_login import UserMixin, LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from sqlalchemy import MetaData
@@ -11,12 +11,19 @@ app = Flask(__name__)
 # LOAD .ENV FILE
 load_dotenv()
 
+# SECRET KEY FOR FLASK FORMS
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['RECAPTCHA_PRIVATE_KEY'] = os.getenv('RECAPTCHA_PRIVATE_KEY')
+app.config['RECAPTCHA_PUBLIC_KEY'] = os.getenv('RECAPTCHA_PUBLIC_KEY')
+
 
 # DATABASE CONFIGURATION
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 app.config['SQLALCHEMY_ECHO'] = True if os.getenv('SQLALCHEMY_ECHO') == 'True' else False
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True if os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS') == 'True' else False
 
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 metadata = MetaData(
     naming_convention={
@@ -34,6 +41,8 @@ migrate = Migrate(app, db)
 
 class User(db.Model, UserMixin):
 
+    __tablename__ = 'users'
+
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), nullable=False, unique=True)
     password = db.Column(db.String(100), nullable=False)
@@ -41,7 +50,6 @@ class User(db.Model, UserMixin):
     lastname = db.Column(db.String(100), nullable=False)
     phone = db.Column(db.String(100), nullable=False)
     role = db.Column(db.String(100), nullable=False)
-    log = db.relationship("Log", uselist=False, back_populates="user")
     bookings = db.relationship('Booking', backref='user', lazy=True)
 
     def __init__(self, email, firstname, lastname, phone, password):
@@ -51,6 +59,10 @@ class User(db.Model, UserMixin):
         self.phone = phone
         self.password = password
         self.role = "end_user"
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
     def check_password(self, password):
 
@@ -64,10 +76,23 @@ class User(db.Model, UserMixin):
 #class Log(db.Model):
 
 
-#class Booking(db.Model, UserMixin):
+class Booking(db.Model, UserMixin):
+
+    __tablename__ = 'bookings'
+
+    id = db.Column(db.Integer, primary_key=True)
+    userid = db.Column(db.Integer, db.ForeignKey('users.id', ondelete='SET NULL'), nullable=True)
+    location = db.Column(db.Text, nullable=False)
+    parkingspotid = db.Column(db.Text, nullable=False)
+    numplate = db.Column(db.Text, nullable=False)
 
 
-#lass City(db.Model, UserMixin):
+class City(db.Model, UserMixin):
+
+    __tablename__ = 'cities'
+
+    id = db.Column(db.Integer, primary_key=True)
+    city = db.Column(db.Text, nullable=False)
 
 
 #class ParkingLot(db.Model, UserMixin):
@@ -76,7 +101,7 @@ class User(db.Model, UserMixin):
 from accounts.views import accounts_bp, passwordHasher
 
 #from dashboard.views import dashboard_bp
-#from booking.views import booking_bp
+from booking.views import booking_bp
 app.register_blueprint(accounts_bp)
 #app.register_blueprint(dashboard_bp)
-#pp.register_blueprint(booking_bp)
+app.register_blueprint(booking_bp)
